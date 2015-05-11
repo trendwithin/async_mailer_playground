a01e1d755384d60ff36dc4d6e6fc77b51c465488
Michael Becco <e4e6d4d5@gmail.com>
2015-05-11 14:45:33 -0700

    Add Test for Mail Delivery

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  include ActiveJob::TestHelper
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
